7d2050fd5e4a43be92a4f0ad0630a923b7c807d9
Mustafa Selcuk Caglar <selcukcaglar08@gmail.com>
2018-10-10 01:01:41 +0300

    01:01

--- Perfect forwarding/main.cpp
+++ Perfect forwarding/main.cpp
@@ -4,11 +4,35 @@
 #include <locale>
 
 /*
-Sorun:
-	Class oluþturularý yapýlýrken sorunlar parametre olarak baþka bir class kullanmak doðal bir oladýr.
-	Fakat parametre olarak oluþturulan classlarýn yapýcý fonksiyonlarý da çalýþýrsa bir fonksiyon çaðýrmak isterken parametre tip sayýsý kadar fonksiyon çaðýrmýþ olacaðýz.
-	Veri defalarca kopyalanmýþ olacak. Veri kopyalanmasýný kaldýrmak üzere std::move(object) kullanýlabilir fakat. Employee
 
+"  Perfect Forwarding "
+C++ zordur ama ...
+C++ diðer programlama dillerinin önüne geçtiði konu kopyalamaya karþý korumadýr. Kopyalama sayýsý program büyüdükçe azalýr. 
+Kopyalama sayýsý azaldýkça hafýza ve CPU kullanýmý azalýr. Kod küçülür ve binary boyutu azalýr.
+Bazen yüzlerce satýr kodu derleyici 10 satýr assembly koduna çevirebilir.
+
+Sorun:
+	Class oluþturularý yapýlýrken parametre olarak baþka bir class kullanmak doðal bir olaydýr.
+	Fakat parametre olarak oluþturulan classlarýn yapýcý fonksiyonlarý da çalýþýrsa bir fonksiyon çaðýrmak isterken parametre tip sayýsý kadar fonksiyon çaðýrmýþ olacaðýz.
+Çözüm:
+	*Veri defalarca geçici kopyalanmýþ olacak. Veri kopyalanmasýný kaldýrmak üzere std::move(object) kullanýlabilir fakat bazýlarý özellikle kopyalama oluþturucusunu çaðýrýrsa
+	mesela(aþaðýda var): " Employee emp1{ "Mustafa", Integer{100} }; " iþte sorun var demektir. Bu durumda dahi veri tipini saklayýp taþýyýcý oluþturucuyu kullanmak için.
+	std::forward<T1>(object) kullanýlýr.
+	*Veri std::forward<T1>(object) ile kopyalanmaz sadece taþýnýr. Rust dilinden haberiniz varsa "ownership" taþýnmýþ olur.
+	
+	Ayrýca fonksiyonunuz olduðunu kabul edin.
+		template <typename T1, typename T2>
+		void wrapper(T1& e1, T2& e2) { func(e1, e2); }
+	Alacaðýnýz saçma hatalar bunlar olacaktýr.
+		wrapper(42, 3.14f);                  // error: invalid initialization of
+											//        non-const reference from
+											//        an rvalue
+
+		wrapper(i, foo_returning_float());   // same error
+
+not1: Visual Studio 2017 Integer classýný tanýmýyor ama derleme ve hata ayýklamada sorun çýkarmýyor. Bir tür hata olsa gerek.
+not2: Visual Studio 2017 C++ derleyicisi son sürümünde optimizasyonu ilerletmiþti. Bu sebeple kopyalama oluþturucularý yerine taþýyýcý oluþturucularý görürseniz þaþmayýn.
+Derleme optimizasyonu hafýza ve hýzý en iyi hale getirmek üzerine yapýlýr. Hýz konusunda optimizasyon denilen þey aslýnda daha az iþ yaptýrmak üzerinedir.
 */
 
 class Employee {
@@ -18,14 +42,14 @@ public:
 	Employee(const std::string &name, const Integer &id) : m_Name{ name }, m_Id{ id } { // Referans kopyalayýcý oluþturucu
 		std::cout << "Employee(const std::string &name, const Integer &id)" << std::endl; 
 	}
-	template<typename T1, typename T2>
-	Employee(T1 &&name, T2 &&id) : m_Name{ (name) }, m_Id{ (id) } { // taþýyýcý oluþturucu. objeyi hafýza da taþýmaya yarar.
-		std::cout << "Employee(std::string &&name,  Integer &&id)" << std::endl;
-	}
-	//template<typename T1, typename T2>
-	//Employee(T1 &&name,  T2 &&id) :	m_Name{ std::forward<T1>(name) }, m_Id{ std::forward<T2>(id) } { // taþýyýcý oluþturucu. objeyi hafýza da taþýmaya yarar.
+	//template<typename T1, typename T2> // Bu fonksiyon 
+	//Employee(T1 &&name, T2 &&id) : m_Name{ (name) }, m_Id{ (id) } { // taþýyýcý oluþturucu. objeyi hafýza da taþýmaya yarar.
 	//	std::cout << "Employee(std::string &&name,  Integer &&id)" << std::endl;
 	//}
+	template<typename T1, typename T2>
+	Employee(T1 &&name,  T2 &&id) :	m_Name{ std::forward<T2>(name) }, m_Id{ std::forward<T2>(id) } { // taþýyýcý oluþturucu. objeyi hafýza da taþýmaya yarar.
+		std::cout << "Employee(std::string &&name,  Integer &&id)" << std::endl;
+	}
 };
 template<typename T1, typename T2>
 Employee *Create(T1 && a, T2 &&b) {
@@ -33,12 +57,12 @@ Employee *Create(T1 && a, T2 &&b) {
 }
 int main() {
 	setlocale(LC_ALL,"Turkish");
+	std::cout << "\n--- Ýþte geliyor. Integer(int), Employee(std::string &&name,  Integer &&id) ---\n" << std::endl;
+	std::string name = "Mustafa"; // "lvalue"
+	Employee emp2{ name, 100 }; // bir kopyalama ve bir taþýma çalýþtý. 2 fonksiyon aktif. Bir nebze daha iyi. 100 "lvalue" olarak geldi.
 	std::cout << "\n--- Ýþte geliyor. Integer(int), Integer(int&&), Employee(std::string &&name,  Integer &&id), ~Integer() ---\n" << std::endl;
 	Employee emp1{ "Mustafa", Integer{100} }; // önce kopyaladý, sonra hareket ettirdi. 4 fonksiyon çalýþtý. Ýstenen durum deðil.
-	std::string name = "Umar"; // "lvalue"
 	//std::cout << "\n--- Ýþte geliyor. ---\n" << std::endl;
-	std::cout << "\n--- Ýþte geliyor. Integer(int), Employee(std::string &&name,  Integer &&id) ---\n" << std::endl;
-	Employee emp2{ name, 100 }; // bir kopyalama ve bir taþýma çalýþtý. 2 fonksiyon aktif. Bir nebze daha iyi. 100 "lvalue" olarak geldi.
 	std::cout << "\n--- Ýþte geliyor. Integer(int) ---\n" << std::endl;
 	Integer val{ 100 }; // Integer kopyalama
 	std::cout << "\n--- Ýþte geliyor. Integer(const Integer&), Employee(std::string &&name,  Integer &&id) ---\n" << std::endl;
--- Variadic Templates/Source.cpp
+++ Variadic Templates/Source.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "Integer.h"
-//Works only for homogenous data types
-//template<typename T>
-//void Print(std::initializer_list<T> args) {
-//	for (const auto &x : args) {
-//		std::cout << x << " ";
-//	}
-//}
 
+//Works only for homogenous data types
+template<typename T>
+void Print(std::initializer_list<T> args) {
+	for (const auto &x : args) {
+		std::cout << x << " ";
+	}
+}
+
+// Base case
 void Print(){
 	std::cout << std::endl;
 }
@@ -16,8 +18,8 @@ void Print(){
 template<typename T, typename...Params>
 //Function parameter pack
 void Print(T &&a, Params&&... args) {
-	//std::cout << sizeof...(args) << std::endl;
-	//std::cout << sizeof...(Params) << std::endl;
+	std::cout << sizeof...(args) << std::endl;
+	std::cout << sizeof...(Params) << std::endl;
 	std::cout << a;
 	if (sizeof...(args) != 0) {
 		std::cout << ",";
@@ -28,8 +30,8 @@ void Print(T &&a, Params&&... args) {
 
 
 int main() {
-	//Print({ 1,2.5,3,4 });
-	//Print(1, 2.5, 3, "4");
+	Print({ 1,2,3,4 });
+	Print(1, 2.5, 3, "4");
 	Integer val{ 1 };
 	Print(0, val, Integer{ 2 });
 	return 0;
